e69cc850ced5f54046126f5ea53a75cfb2e36251
t3nderex <t3nderex@gmail.com>
2022-05-02 15:43:23 +0900

    [feat]: 매개변수 처리 로직 추가

--- explorer.py
+++ explorer.py
@@ -35,3 +35,51 @@ def get_ft(file_name):
         print(f"file type:{file_type}")
     
     return file_type 
+
+
+# 현재 디렉터리 탐색
+def search_directroy(directory):
+    pass
+
+
+# 정규식을 통한 파일 내 개인정보 탐색 
+def search_pi(file):
+    type = get_ft(file)
+    pass
+
+
+if __name__ == '__main__':    
+    if len(argv) == 1:
+        print("-> [specific reason] 실행인자가 없습니다. \'-h, -help\'를 통해 사용법을 확인하세요.")
+        exit()
+
+    elif len(argv) == 2:
+        if argv[1] == '-h' or '-help':
+            print('-r : 개인정보를 탐색할 범위를 지정합니다. \'-r [file]or[dircetory]\' 형태로 사용합니다.')
+            exit()
+
+        if argv[1] == '-r' or '-range':
+            print("전체 디렉터리")
+    
+    elif len(argv) == 3:
+        if argv[1] == '-r' or '-range':
+            if argv[2] == 'all':
+                print('전체 경로 탐색을 시작합니다.')
+                # 탐색 함수 추가
+            else:
+                if os.path.isfile(argv[2]) is True:
+                    print(f'{argv[2]}파일을 탐색합니다.')
+                    # 탐색 함수 추가
+                elif os.path.isfile(argv[2]) is False:
+                    print(f'{argv[2]} 디렉터리를 탐색합니다.')
+                    # 탐색 함수 추가                          
+                else:
+                    print('알 수 없는 파일 또는 디렉터리 입니다.')
+                    exit()
+
+    else:
+        print('[exit] wrong arguments.')
+        exit()
+# python dev.py -r all
+# python dev.py -r "[dir_path]"
+# python dev.py -r "[file_path]"
