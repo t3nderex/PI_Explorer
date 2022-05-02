38b9d2292ecf5232a1561508ad92af38dc02b2a8
t3nderex <t3nderex@gmail.com>
2022-05-02 18:39:23 +0900

    feat: pi_explorer 함수 정의

--- explorer_lib/pi_explorer.py
+++ explorer_lib/pi_explorer.py
@@ -1,14 +1,36 @@
-
-# 현재 디렉터리 탐색
-
-
-
-# 정규식을 통한 파일 내 PI(Personal Information) 탐색
-# 탐색된 개인정보를 딕셔너리로 반환 
-# rtype = dict
+"""
+    TODO:
+        * 파일 시그니쳐 별 처리  
+"""
 def find_pi(file_name, file_type):
     if file_type == 'Hangul (Korean) Word Processor File 5.x':
         pass
     searched_pi = True
     
     return searched_pi
+
+
+def find_pi_hwp():
+    pass
+
+
+def find_pi_word():
+    pass
+
+
+def find_pi_pdf():
+    pass
+
+
+def find_pi_excel():
+    pass
+
+
+def find_pi_ppt():
+    pass
+
+
+def find_pi_txt():
+    pass
+
+    
