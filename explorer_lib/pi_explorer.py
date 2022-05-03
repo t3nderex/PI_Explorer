6915958584d6c823b8f932f1fe4bc776a52f62b2
t3nderex <t3nderex@gmail.com>
2022-05-03 18:52:36 +0900

    FEAT: find_pi() 파일 타입에 따른 조건문 처리 추가

--- explorer_lib/pi_explorer.py
+++ explorer_lib/pi_explorer.py
@@ -1,36 +1,78 @@
-"""
-    TODO:
-        * 파일 시그니쳐 별 처리  
-"""
-def find_pi(file_name, file_type):
-    if file_type == 'Hangul (Korean) Word Processor File 5.x':
-        pass
-    searched_pi = True
+import olefile 
+import re
+
+
+
+def find_pi(file_name, file_type, regexp_li):
+    regexp_li = regexp_li
     
-    return searched_pi
-
-
-def find_pi_hwp():
-    pass
-
-
-def find_pi_word():
-    pass
-
-
-def find_pi_pdf():
-    pass
-
-
-def find_pi_excel():
-    pass
-
-
-def find_pi_ppt():
-    pass
-
-
-def find_pi_txt():
-    pass
-
     
+    
+
+    if 'Hangul' in file_type: 
+        result = find_pi_hwp(file_name)
+        return result
+
+    elif 'doc' in file_type:
+        result = find_pi_msword(file_name)
+        return result
+
+    elif 'pdf' in file_type:
+        result = find_pi_pdf(file_name)
+        return result
+
+    elif 'excel' or 'csv' in file_type:
+        result = find_pi_excel(file_name)
+        return result
+
+    elif 'doc' in file_type:
+        result = find_pi_ppt(file_name)
+        return result
+
+    elif 'doc' in file_type:
+        result = find_pi_txt(file_name)
+        return result
+
+    else:
+        print(f'{file_type}은 지원되지 않는 문서 형식 입니다.')
+        return False
+        
+    
+    
+    
+
+
+def find_pi_hwp(file_name):
+    file = olefile.OleFileIO(file_name)
+    encoded_text = file.openstream('PrvText').read()
+    decoded_text = encoded_text.decode('utf-16')
+    
+    
+    
+
+def find_pi_msword(file_name):
+    pass
+
+
+def find_pi_pdf(file_name):
+    pass
+
+
+def find_pi_excel(file_name):
+    pass
+
+
+def find_pi_ppt(file_name):
+    pass
+
+
+def find_pi_txt(file_name):
+    pass
+
+
+
+if __name__ == '__main__':
+    file_name = './test/test.hwp'
+    file_type ='Hangul'
+
+    find_pi(file_name, file_type)
